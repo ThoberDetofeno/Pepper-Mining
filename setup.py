fde9847dc3937f3685d9c026eb4d8bfcd9795745
Thober Detofeno <124348305+ThoberDetofeno@users.noreply.github.com>
2023-04-18 06:12:59 -0300

    Pepper Mining Alpha version
    Update the setup files

--- setup.py
+++ setup.py
@@ -1,35 +1,31 @@
 from setuptools import find_packages, setup
 
-LONG_DESCRIPTION = 'PepperMining is a open source process mining platform written in Python.'
-
-with open("README.md", "r", encoding="utf-8") as fh:
+with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
-setup(name="peppermining",
-      version="0.0.1",
-      author="Thober Detofeno",
-      author_email="thober@gmail.com",
-      description="Pepper Mining is a free process mining.",
-      long_description=long_description,
+setup(name='peppermining',
+      version='0.1.0',
+      author='Thober Detofeno',
+      author_email='thober@gmail.com',
+      description="PepperMining is a free Process Mining framework.",
+      long_description="PepperMining is a open source Process Mining platform written in Python.",
       license='MIT',
-      long_description_content_type="text/markdown",
-      # TODO adicione outros pacotes que precisem ser instalados com o seu pacote. Ex: 'caer'
-      # pip install pydot
+      long_description_content_type='text/markdown',
       install_requires=['numpy', 'pandas', 'pydot', 'deepdiff'],
-      url="https://github.com/ThoberDetofeno/peppermining",
+      url='https://github.com/ThoberDetofeno/peppermining',
       project_urls={
-          "Bug Tracker": "https://github.com/ThoberDetofeno/peppermining/issues",
-          "repository": "https://github.com/ThoberDetofeno/peppermining/tree/master",
+          'Bug Tracker': 'https://github.com/ThoberDetofeno/peppermining/issues',
+          'repository': 'https://github.com/ThoberDetofeno/peppermining/tree/master',
       },
       keywords=['python', 'process mining'],
       classifiers=[
-          "Development Status :: 1 - Planning",
-          "Programming Language :: Python :: 3",
-          "License :: OSI Approved :: MIT License",
-          "Operating System :: OS Independent",
+          'Development Status :: 1 - Planning',
+          'Programming Language :: Python :: 3',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
       ],
-      packages=find_packages(include=["src"]),
+      packages=find_packages(include=["peppermining*"]),
       setup_requires=['pytest-runner'],
       tests_require=['pytest==4.4.1'],
       test_suite='tests',
-      python_requires=">=3.8",)
+      python_requires='>=3.8',)
