3c523e8adf70f109da8e9ddae25a9801be743794
Thober Detofeno <124348305+ThoberDetofeno@users.noreply.github.com>
2023-04-25 04:13:28 -0300

    Add files via upload

--- peppermining/__init__.py
+++ peppermining/__init__.py
@@ -1,9 +1,34 @@
 from peppermining import utils
-from peppermining import kpi
-from peppermining import filters
-from peppermining import conformance
-from peppermining.conformance import violation
-
 from peppermining.pepper import Pepper
 from peppermining.peppermining import PepperMining
+
+from peppermining import kpi
+from peppermining.kpi.average_events_per_case import AverageEventsPerCase
+from peppermining.kpi.number_of_activities import NumberOfActivities
+from peppermining.kpi.number_of_cases import NumberOfCases
+from peppermining.kpi.number_of_events import NumberOfEvents
+from peppermining.kpi.rework import Rework
+from peppermining.kpi.throughput_time import ThroughputTime
+
+from peppermining import filters
+from peppermining.filters.case_activity_filter import CaseActivityFilter
+from peppermining.filters.case_between_time_filter import CaseBetweenTimeFilter
+from peppermining.filters.case_end_activity_filter import CaseEndActivityFilter
+from peppermining.filters.case_filter import CaseFilter
+from peppermining.filters.case_size_filter import CaseSizeFilter
+from peppermining.filters.case_start_activity_filter import CaseStartActivityFilter
+from peppermining.filters.variant_filter import VariantFilter
+
+from peppermining import conformance
+from peppermining.conformance.conformance import Conformance
 from peppermining.conformance.process_model import ProcessModel
+from peppermining.conformance.root_cause_analysis import root_cause_analysis
+
+from peppermining.conformance import violation
+from peppermining.conformance.violation.run_by_same_user import RunBySameUser
+from peppermining.conformance.violation.undesired_activity import UndesiredActivity
+from peppermining.conformance.violation.undesired_connection import UndesiredConnection
+from peppermining.conformance.violation.undesired_end import UndesiredEnd
+from peppermining.conformance.violation.undesired_start import UndesiredStart
+
+
